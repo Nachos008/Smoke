e83c5b0b18d4e8b8ff4921eac4b9ccacee11dfe7
hoopjakub <jakub.kahanec@gmail.com>
2025-03-12 20:16:11 +0100

    los indexos

---
+++ Smoke/resources/views/library.blade.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <div class="uplnehore">
+        <img src="a2.png" alt="Company Logo" class="company-logo">
+        <div class="uplnehoretxt"></div>
+        <p class="vedlabuttontxt">If you have any questions, come and ask them here</p>
+        <a href="https://discord.gg/35KuMCwq" class="link">
+         <button type="button" class="button">
+             <span class="buttontxt">Comunity</span>
+             <svg class="dslogo" width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M15.2112 1.43874C14.1185 0.927171 12.9469 0.550269 11.7218 0.334402C11.6995 0.330235 11.6772 0.340647 11.6657 0.36147C11.5151 0.634947 11.3482 0.99172 11.2313 1.27214C9.91365 1.07086 8.6028 1.07086 7.31221 1.27214C7.1953 0.985487 7.02233 0.634947 6.87097 0.36147C6.85947 0.341342 6.83719 0.33093 6.81488 0.334402C5.59051 0.549579 4.41885 0.926481 3.32556 1.43874C3.3161 1.4429 3.30798 1.44985 3.3026 1.45887C1.08021 4.84685 0.471401 8.15155 0.770061 11.4153C0.771412 11.4312 0.780196 11.4465 0.792359 11.4562C2.25863 12.555 3.67896 13.2221 5.07292 13.6642C5.09523 13.6711 5.11887 13.6628 5.13307 13.6441C5.46281 13.1846 5.75674 12.7001 6.00876 12.1906C6.02364 12.1608 6.00944 12.1254 5.97904 12.1136C5.51281 11.9331 5.06887 11.713 4.64182 11.4632C4.60805 11.443 4.60534 11.3937 4.63642 11.3701C4.72628 11.3014 4.81617 11.2299 4.90198 11.1577C4.91751 11.1446 4.93914 11.1418 4.95739 11.1501C7.76289 12.4571 10.8002 12.4571 13.5726 11.1501C13.5908 11.1411 13.6124 11.1439 13.6286 11.1571C13.7145 11.2292 13.8043 11.3014 13.8949 11.3701C13.926 11.3937 13.9239 11.443 13.8902 11.4632C13.4631 11.7179 13.0192 11.9331 12.5523 12.1129C12.5219 12.1247 12.5083 12.1608 12.5232 12.1906C12.7806 12.6994 13.0746 13.1839 13.3982 13.6434C13.4118 13.6628 13.4361 13.6711 13.4584 13.6642C14.8591 13.2221 16.2794 12.555 17.7457 11.4562C17.7585 11.4465 17.7667 11.4319 17.768 11.416C18.1254 7.64274 17.1693 4.36514 15.2334 1.45956C15.2287 1.44985 15.2206 1.4429 15.2112 1.43874ZM6.42772 9.42801C5.58307 9.42801 4.88711 8.63673 4.88711 7.66496C4.88711 6.69319 5.56958 5.90191 6.42772 5.90191C7.2926 5.90191 7.98183 6.70014 7.96831 7.66496C7.96831 8.63673 7.28584 9.42801 6.42772 9.42801ZM12.1239 9.42801C11.2792 9.42801 10.5833 8.63673 10.5833 7.66496C10.5833 6.69319 11.2657 5.90191 12.1239 5.90191C12.9888 5.90191 13.678 6.70014 13.6645 7.66496C13.6645 8.63673 12.9888 9.42801 12.1239 9.42801Z" fill="#5865F2"/>
+             </svg>
+         </button>
+        </a>
+ 
+        <span class="company-name">GamingForAll.xyz</span>
+     </div>
+ 
+     <div class="ciaradiv1">
+         <div class="ciara1"></div>
+     </div>  
+ 
+     <nav>
+         <a href="index.html">Home</a>
+         <div class="dropdown">
+           <a href="#">Games</a>
+           <div class="dropdown-content">
+             <a href="#">Project Zomboid</a>
+             <a href="#">Player Unknown Batlle Grounds</a>
+             <a href="#">Ready Or Not</a>
+             <a href="#">Lethal Company</a>
+           </div>
+         </div>
+         <a href="index3.html">Q&A</a>
+       </nav>
+
+       <div id="libraryContainer">
+        <h1>Game Library</h1>
+        <h2>Name:</h2>
+        <article class="projectZ">
+          <img class=novehryimg src="Boxshot_of_video_game_Project_zomboid.jpg" alt="Game 1">
+          <div class="text-content1">
+            <h2>Project Zomboid</h2>
+        </div>
+        </article>
+        <article class="pubg">
+          <img class=novehryimg src="44b6bfcd7956199bd50eb156219292e80b76a108d6ac4c6e.avif" alt="Game 2">
+          <div class="text-content1">
+            <h2>PlayerUnknown's Battlegrounds</h2>
+        </div>
+        </article>
+        <article class="ron">
+          <img class=novehryimg src="MV5BODU3NDFjZjktNzA0Ni00ZTFmLWIzN2ItNDZlOGYwOTAwMmQ3XkEyXkFqcGdeQXVyMTM2MTExNDIx._V1_FMjpg_UX1000_.jpg" alt="Game 3">
+          <div class="text-content1">
+            <h2>Ready Or Not</h2>
+        </div>
+        </article>
+        <article class="lethalC">
+          <img class=novehryimg src="capsule_616x353.jpg" alt="Game 4">
+          <div class="text-content1">
+            <h2>Lethal Company</h2>
+        </div>
+      </article>
+        <article class="fc24">
+          <img class=novehryimg src="download.jpeg" alt="Game 5">
+          <div class="text-content1">
+            <h2>FC 24</h2>
+        </div>
+        </article>
+        <article class="Cult">
+          <img class=novehryimg src="Cult_of_the_Lamb_Key_Art.png" alt="Game 6">
+          <div class="text-content1">
+            <h2>Cult of the Lamb</h2>
+        </div>
+        </article>
+        <article class="Hell">
+          <img class=novehryimg src="Hell_Let_Loose_cover_art.jpg" alt="Game 7">
+          <div class="text-content1">
+            <h2>Hell Let Loose</h2> 
+        </div>
+        </article>
+    </div>
+
+    <button id="homeButton" onclick="scrollToTop()">Home</button>
+
+      <script>
+        function scrollToTop() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+      </script>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer>
+        <div>
+            <p class="footer-content">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                
+            </p>
+            <p class="social-media">
+                Our Social Media:
+            </p>
+        <div class="social-media-links">
+          <a href="https://open.spotify.com/playlist/1jYgPon3Aj0WHocnUI5G4G" target="_blank" class="social-link1"><img src="spotify-2-logo-png-transparent.png" alt="Spotify"></a>
+          <a href="index3.html" target="_blank" class="social-link2"><img src="580b57fcd9996e24bc43c53e.png" alt="Twitter"></a>
+          <a href="https://www.instagram.com/hoopjakub/" target="_blank" class="social-link3"><img src="Instagram.svg.png" alt="Instagram"></a>
+          <a href="index3.html" target="_blank" class="social-link4"><img src="discord-icon-svgrepo-com.svg" alt="Discord"></a>
+        </div>
+       </div>
+      </footer>
+</body>
+</html>
