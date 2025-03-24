a12fddbaee85dfc35bb196d63b50ad80f9c84235
hoopjakub <jakub.kahanec@gmail.com>
2025-03-24 09:35:18 +0100

    small update games

--- Smoke/app/Models/Games.php
+++ Smoke/app/Models/Games.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Games extends Model
 {
-    //in database is: game_id, title, description, price, cover_image, created_at, publisher, developer, release_date
+    //in database is: game_id, title, description, price, cover_image, created_at, publisher, developer, release_date, owned, pge_rating, review_score
 
     //more to add (example): ALTER TABLE games ALTER TABLE games ADD COLUMN rating DECIMAL(3,1) NULL,ADD COLUMN genre VARCHAR(100) NULL,ADD COLUMN platform VARCHAR(100) NULL; 
 
