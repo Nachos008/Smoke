570973c95c6673cc89827a4a5a1d4a4d16021153
Nachos008 <patko.meszaros1@gmail.com>
2025-03-27 09:37:09 +0100

    1

--- Smoke/resources/views/library.blade.php
+++ Smoke/resources/views/library.blade.php
@@ -1,5 +1,37 @@
 @extends('layout.layout')
 @section('content')   
 <link rel="stylesheet" href="{{asset('styles/library.css')}}">
-       
+
+<div class="library">
+    <div class="sidebar">
+        <h2>Library</h2>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+            <li>Item 6</li>
+            <li>Item 7</li>
+            <li>Item 8</li>
+            <li>Item 9</li>
+            <li>Item 10</li>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+            <li>Item 6</li>
+            <li>Item 7</li>
+            <li>Item 8</li>
+            <li>Item 9</li>
+            <li>Item 10</li>
+        </ul>
+    </div>
+    <div class="content">
+
+    </div>
+    
+</div>    
+
 @endsection
